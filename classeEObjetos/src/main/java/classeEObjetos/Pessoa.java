685dd6628a2d4e2fc3d75eba6227cfeaafd1b446
Igor Castelo <freitascasteloigor@gmail.com>
2023-02-09 21:53:59 -0300

    foi criado a classe Pessoa e o objeto objetoPessoa foi intanciado na funcao pricial.

---
+++ classeEObjetos/src/main/java/classeEObjetos/Pessoa.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classeEObjetos;
+
+/**
+ *
+ * @author freit
+ */
+public class Pessoa {
+    String nome;
+    int idade;
+    
+    void correr(){
+    System.out.println("Correndo!!!");
+    }
+    
+    void comer(String comida){
+    System.out.println("Comendo: " + comida);
+    }
+}
